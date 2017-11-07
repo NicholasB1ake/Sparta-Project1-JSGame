d8a963b1b827876550a84819ad26a94bcd6d74bb
nick <nicholasb1ake@hotmail.com>
2017-11-07 18:36:45 +0000

    frog moves

--- main.js
+++ main.js
@@ -1,68 +1,120 @@
 $(document).ready(function(){
-    let grid = $('td')
-  // $('#frogger1').click(function(){
-  //   $('#frogger1').hide();
-  // });
+    var grid = $('td')
+    var frogIndex = 9;
 
-$(document).keydown(function(e) {
-    switch(e.which) {
+
+  var count = 60, timer = setInterval(function() {
+      count--;
+      $("#demo").html(count);
+      if (count === 2)
+        alert('you loose');
+
+      if(count == 1) clearInterval(timer);
+  }, 1000);
+
+
+  $(document).keydown(function(event) {
+    switch(event.which) {
           case 37:
-          forward()
-
-          //$('#frogger1').hide();
-          console.log('left');
+          left(event);
+      console.log('left');
           break;
-
           case 38:
-          console.log('up');
-
+          forward(event);
           break;
 
           case 39:
+          right(event);
           console.log('right');
           // right
           break;
-
           case 40: // down
+            backwards(event)
           console.log('down');
           break;
 
           default: return; // exit this handler for other keys
       }
-      e.preventDefault(); // prevent the default action (scroll / move caret)
+      event.preventDefault(); // prevent the default action (scroll / move caret)
   });
 
-  $(document).keyup(function(e) {
-      switch(e.which) {
-            case 37:
-            $('#frogger1').show();
-            console.log('left');
-            break;
+  function forward(event){
+    var fIndex = frogIndex;
+    var oldFrog = 'td#' + fIndex + ' img';
 
-            case 38:
-            console.log('up');
+    $(oldFrog).remove()
 
-            break;
+    //current index + 20;
+    var newfIndex = fIndex + 20;
+    frogIndex = newfIndex;
+    var element = 'td#' + newfIndex;
+    $(element).html('<img id="frogger1" src="images/froggerSpriteOne1.png" alt="">');
+    checkForWin(newfIndex);
+};
 
-            case 39:
-            console.log('right');
-            // right
-            break;
+function left(event){
+  var fIndex = frogIndex;
+  var oldFrog = 'td#' + fIndex + ' img';
 
-            case 40: // down
-            console.log('down');
-            break;
+  $(oldFrog).remove()
 
-            default: return; // exit this handler for other keys
-        }
-        e.preventDefault(); // prevent the default action (scroll / move caret)
-    });
+  //current index + 20;
+  var newfIndex = fIndex - 1;
+  debugger
 
-//$(this).on("keypress", function())
-});
+  frogIndex = newfIndex;
 
-function forward(){
-  console.log('')
+  debugger;
+
+  var element = 'td#' + newfIndex;
+
+  $(element).html('<img id="frogger1" src="images/froggerSpriteOne1.png" alt="">');
+};
+
+function right(event){
+  var fIndex = frogIndex;
+  var oldFrog = 'td#' + fIndex + ' img';
+
+  $(oldFrog).remove()
+
+  //current index + 20;
+  var newfIndex = fIndex + 1;
+  debugger
+
+  frogIndex = newfIndex;
+
+
+
+  var element = 'td#' + newfIndex;
+
+  $(element).html('<img id="frogger1" src="images/froggerSpriteOne1.png" alt="">');
+};
+
+function backwards(event){
+  var fIndex = frogIndex;
+  var oldFrog = 'td#' + fIndex + ' img';
+
+  $(oldFrog).remove()
+
+  //current index + 20;
+  var newfIndex = fIndex - 20;
+
+
+  frogIndex = newfIndex;
+
+
+
+  var element = 'td#' + newfIndex;
+
+  $(element).html('<img id="frogger1" src="images/froggerSpriteOne1.png" alt="">');
+
+};
+
+function checkForWin(fIndex){
+  debugger;
+  if ('td#' + fIndex === 'td#189'){
+    alert('sdfsdf');
+  }
 }
 
 
@@ -72,12 +124,19 @@ function forward(){
 
 
 
-// $('#frogger1').click(function(){
-//   $('#frogger1').hide()
 
 
-// let arrayPosition =
-//
-// for(let )
+
+});
+
+
+
+
+
+
+
+
+
+
 
 //});
