b05bbc11148d716800c9af7b95a866aa23d2afdc
nick <nicholasb1ake@hotmail.com>
2017-11-07 11:40:11 +0000

    cursor buttons working

--- main.js
+++ main.js
@@ -1,1 +1,83 @@
+$(document).ready(function(){
+    let grid = $('td')
+  // $('#frogger1').click(function(){
+  //   $('#frogger1').hide();
+  // });
 
+$(document).keydown(function(e) {
+    switch(e.which) {
+          case 37:
+          forward()
+
+          //$('#frogger1').hide();
+          console.log('left');
+          break;
+
+          case 38:
+          console.log('up');
+
+          break;
+
+          case 39:
+          console.log('right');
+          // right
+          break;
+
+          case 40: // down
+          console.log('down');
+          break;
+
+          default: return; // exit this handler for other keys
+      }
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
+  $(document).keyup(function(e) {
+      switch(e.which) {
+            case 37:
+            $('#frogger1').show();
+            console.log('left');
+            break;
+
+            case 38:
+            console.log('up');
+
+            break;
+
+            case 39:
+            console.log('right');
+            // right
+            break;
+
+            case 40: // down
+            console.log('down');
+            break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+//$(this).on("keypress", function())
+});
+
+function forward(){
+  console.log('')
+}
+
+
+
+
+
+
+
+
+// $('#frogger1').click(function(){
+//   $('#frogger1').hide()
+
+
+// let arrayPosition =
+//
+// for(let )
+
+//});
